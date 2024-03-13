aee935f0952e096571db65e3d40b34bbf495af4a
k-ankit <nvsankit2020@gmail.com>
2024-03-13 21:46:06 +0530

    client folder structure created

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className=' text-xl text-purple-800 bg-gray-100 h-screen w-full flex justify-center items-center'>
+     hellow world
+    </div>
+  )
+}
+
+export default App
