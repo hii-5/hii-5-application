ba3bbddc9b0de0c3d581628caaa5211fc0283408
Praveen Kumar <praveennqumar>
2024-04-10 00:06:58 +0530

    some change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
+import { useState } from "react";
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
-    <div className=' text-xl text-purple-800 bg-gray-100 h-screen w-full flex justify-center items-center'>
-     checking the merge request
+    <div className=" text-xl text-purple-800 bg-gray-100 h-screen w-full flex justify-center items-center">
+      checking the merge request by praveen kumar
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
